310ccd4d00797e55ebe003b86578c0a32eaec4b6
你的名字 <@example.com>
2026-05-18 22:12:08 +0800

    货架，货架层，包裹结构体创建

--- 1.c
+++ 1.c
@@ -5,6 +5,156 @@
 #include <time.h>
 
 
+void initRandomSeed() {
+    srand((unsigned int)time(NULL));
+}
+
+
+void generatePickupCode(char* code) {
+    for (int i = 0; i < 5; i++) {
+        code[i] = '0' + rand() % 10;
+    }
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += code[i] - '0';
+    }
+    code[5] = '0' + (sum % 10);
+    code[6] = '\0';
+}
+
+
+
+Package *CreatPac(const char *ID,const char *name,const char *phone)
+{
+    Package *newPac = malloc(sizeof(Package));
+    if (newPac == NULL)
+    {
+        return NULL;
+    }
+    strcpy(newPac->ID,ID);
+    strcpy(newPac->name,name);
+    strcpy(newPac->phone,phone);
+    generatePickupCode(newPac->code);
+    newPac->next = NULL;
+    
+    return newPac;
+}
+
+ShelfLevel *CreatLevel(int levelnum,int maxsize)
+{
+    ShelfLevel *level = malloc(sizeof(ShelfLevel));
+    if (level == NULL)
+    {
+        return NULL;
+    }
+    level->LevNum = levelnum;
+    level->MaxSize = maxsize;
+    level->CurSize = 0;
+    level->HeadPac = NULL;
+    level->next = NULL;
+    
+    return level;
+}
+
+Shelf *CreatShelf(int shelfnum,int maxsize)
+{
+    Shelf *shelf = malloc(sizeof(Shelf));
+    if (shelf == NULL)
+    {
+        return NULL;
+    }
+    shelf->ShelfNum = shelfnum;
+    shelf->MaxSize = maxsize;
+    shelf->HeadLevel = NULL;
+    shelf->next = NULL;
+    
+    for (int i = 0; i < BIGLEVELNUM; i++)
+    {
+        ShelfLevel *newlevel = CreatLevel(i,maxsize);
+        if(shelf->HeadLevel ==NULL)
+        {
+            shelf->HeadLevel = newlevel;
+        }
+        else
+        {
+            ShelfLevel *temp = shelf->HeadLevel;
+            while (temp!=NULL)
+            {
+                temp=temp->next;
+            }
+            newlevel = temp;
+        }
+    }
+    return shelf;
+}
+
+Shelf *shelfhead=NULL;
+
+void addshelf(Shelf *shelf)
+{
+    for (int i = 0; i < BIGSHELFNUM; i++)
+    {
+        if (shelfhead->next == NULL)
+        {
+            shelfhead->next = shelf;
+        }
+        else
+        {
+            Shelf *temp = shelfhead;
+            while (temp != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = shelf;
+        }
+    }
+    return shelf;
+}
+
+
+ShelfLevel *findlevel(Shelf *shelf,int levelnum)
+{
+    ShelfLevel *level = shelf->HeadLevel;
+    while (level!=NULL)
+    {
+        if (level->LevNum ==levelnum)
+        {
+            return level;
+        }
+        level = level->next;
+    }
+    return NULL;
+}
+
+Shelf *findshelf(int shelfnum)
+{
+    Shelf *shelf = shelfhead->next;
+    while (shelf!=NULL)
+    {
+        if (shelf->ShelfNum = shelfnum)
+        {
+            return shelf;
+        }
+        shelf = shelf->next;
+    }
+    return NULL;
+}
+
+
+void PacStore(Package *newPac)
+{
+    printf("请输入入库货架号：");
+
+
+}
+
+
+
+
+
+
+
+
 int main()
 {
     srand(time(NULL));
--- 1.h
+++ 1.h
@@ -1,116 +1,41 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include <ctype.h>
+
 #define MAXSIZE 100
 #define CODE 6
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <ctype.h>
+#define LEVELMAXSIZE 20
+#define BIGLEVELNUM 5
+#define BIGSHELFNUM 10
 
 typedef struct package
 {
-    char ID;
-    char phone[15];
+    char ID[MAXSIZE];
     char name[MAXSIZE];
+    char phone[12];
     char code[CODE];
-    int posNum;
-    int shelfNum;
-    struct package*next;
-}Package;           //包裹结构体
+    struct package *next;
 
-//栈
-typedef struct position
+}Package;
+
+typedef struct shelflevel
 {
-    Package *top;
-    int count;
-}Position;
+    int LevNum;
+    int MaxSize;
+    int CurSize;
+    Package *HeadPac;
+    struct shelflevel *next;
+}ShelfLevel;
 
-//全局变量
-Position shelves[10][20];       //货架
-int totalpackage;
-
-//初始化货架
-int initshelves()
+typedef struct shelf
 {
-    for (int i = 0; i < 11; i++)
-    {
-        for (int j = 0; j < 21; i++)
-        {
-            shelves[i][j].top = NULL;
-            shelves[i][j].count = 0;
-        }
-    }
-}
-
-void generateCode(char *code)
-{
-    for (int i = 0; i < CODE; i++)
-    {
-        code[i] = '0' + rand() % 10;
-    }
-    code[7] = '\0';
-}
+    int ShelfNum;
+    int MaxSize;
+    ShelfLevel *HeadLevel;
+    struct shelf *next;
+}Shelf;
 
 
-//包裹入库
-void storePackage()
-{
-    char name[MAXSIZE];
-    char phone[15];
-    Package *newPac = malloc(sizeof(Package));
-    printf("请输入客户的名字：");
-    scanf("%s",name);
-    printf("请输入客户的电话号码：");
-    scanf("%s",phone);
-
-    printf("包裹存放货架位置：");
-    scanf("%d",&newPac->posNum);
-    printf("包裹所在在货架位置：");
-    scanf("%d",&newPac->shelfNum);
-    strcpy(newPac->name,name);
-    strcpy(newPac->phone,phone);
-
-    generateCode(newPac->code);         //随机取件码
-
-    newPac->ID = ++totalpackage;
-
-    Position *pos = &shelves[newPac->posNum-1][newPac->shelfNum-1];
-    newPac->next = pos->top;
-    pos->top = newPac;
-    pos->count++;
-
-    printf("包裹已入库\n");
-    printf("包裹编号为:%d\n",newPac->ID);
-    printf("姓名：%s\n",newPac->name);
-    printf("电话：%s\n",newPac->phone);
-    printf("所在位置：%d号货架 %d号柜\n",newPac->posNum,newPac->shelfNum);
-    printf("取件码：%s\n",newPac->code);
-}
 
 
-// 取件操作：输入取件码
-void pickupPackage()
-{
-    char code[CODE];
-    printf("请输入取件码：");
-    scanf("%s",code);
-    Package *current = malloc(sizeof(Package));
-    Package *prev = NULL;
-    for (int i = 0; i < 11; i++)
-    {
-        for (int j = 0; j < 21; j++)
-        {
-            if(shelves[i][j].top!=NULL)
-            {
-                current = &shelves[i][j];
-                if (current->code == code)
-                {
-                    
-                }
-                
-            }
-            
-        }
-        
-    }
-    
 
-}
